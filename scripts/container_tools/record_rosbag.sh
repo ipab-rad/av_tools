ca0dcf83f8019bddb242003490d0e64f3b478e14
Hector Cruz <hcruzgo@ed.ac.uk>
2024-05-07 14:37:08 +0100

    Split rosbags in 10 gb

--- scripts/container_tools/record_rosbag.sh
+++ scripts/container_tools/record_rosbag.sh
@@ -16,6 +16,7 @@ if [ "$#" -eq 0 ]; then
     echo -e "No topics list file provided. ${CYAN}Recording all topics.${NO_COLOR}"
     ros2 bag record -s mcap --all --max-cache-size 1048576000 \
         --storage-config-file "$ROS_WS/config/mcap_cfg.yaml" \
+        -d 10740000000 \
         -o "$OUTPUT_DIR/${DATE_PREFIX}_sensor_recording"
     exit 0
 elif [ "$#" -ne 1 ]; then
@@ -43,5 +44,6 @@ else
     echo -e "Recording topcis from ${MAGENTA}$TOPICS_LIST_FILE${NO_COLOR}"
     ros2 bag record -s mcap --max-cache-size 1048576000 \
         --storage-config-file "$ROS_WS/config/mcap_cfg.yaml" \
+        -d 10740000000 \
         -o "$OUTPUT_DIR/${DATE_PREFIX}_sensor_recording" "${TOPICS[@]}"
 fi
