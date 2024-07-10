691adbcb791cb884148aa8d0bee93eeeecf75011
hect95 <hector_cruz95@live.com>
2024-07-10 11:22:00 +0100

    Add option to define rosbags dir

--- runtime.sh
+++ runtime.sh
@@ -3,6 +3,50 @@
 # Build docker image and run ROS code for runtime or interactively with bash
 # ---------------------------------------------------------------------------
 
+# Default in-vehicle rosbags directory
+ROSBAGS_DIR=/recorded_datasets/edinburgh
+
+# Function to print usage
+usage() {
+    echo "Usage: runtime.sh [--path | -p ] [--help | -h]"
+    echo ""
+    echo "Options:"
+    echo "  --path, -p ROSBAGS_DIR_PATH"
+    echo "                 Specify path to store recorded rosbags"
+    echo "  --help, -h     Display this help message and exit."
+    echo ""
+}
+
+# Parse command-line options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+            # Option to specify path
+        -p|--path)
+            if [ -n "$2" ]; then
+                ROSBAGS_DIR="$2"
+                shift
+            else
+                echo "Error: Argument for $1 is missing."
+                usage
+            fi
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+    shift
+done
+
+# Verify ROSBAGS_DIR exists
+if [ ! -d "$ROSBAGS_DIR" ]; then
+    echo "$ROSBAGS_DIR does not exist! Please provide a valid path to store rosbags"
+    exit 1
+fi
+
 # Initialise CMD as empty
 CMD=""
 
@@ -16,16 +60,10 @@ DOCKER_BUILDKIT=1 docker build \
     -t av_tools:latest \
     -f Dockerfile --target runtime .
 
-# Get the absolute path of the script
-SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-
-# Create a dir to store rosbags
-mkdir -p $SCRIPT_DIR/rosbags
-
 # Run docker image
 docker run -it --rm --net host --privileged \
     -v /dev:/dev \
     -v /tmp:/tmp \
-    -v /recorded_datasets/edinburgh:/opt/ros_ws/rosbags \
+    -v $ROSBAGS_DIR:/opt/ros_ws/rosbags \
     -v /etc/localtime:/etc/localtime:ro \
     av_tools:latest $CMD
